127a8770dcee2db78c74d46eeb4b27fb2bdac5f2
Seungyeon-Lee <yeondiary@gmail.com>
2020-09-24 10:35:21 +0000

    Deploying to gh-pages from  @ 5f2df16cc9dce3eb2dcd7ace5c940223ef089c95 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4573c19a9c4991e036b05bd0b5bc3c0d.js"
+  "/precache-manifest.267ba9cbe49d0d70a44cb06188ea97c9.js"
 );
 
 self.addEventListener('message', (event) => {
